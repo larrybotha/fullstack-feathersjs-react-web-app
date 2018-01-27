2a420ca4f650c3854470f5971b7d43a9d7ca9f48
Larry Botha <larry@fixate.it>
2018-01-27 20:31:40 +0200

    create app

---
+++ client/src/index.js
@@ -0,0 +1,20 @@
+import {h, Component} from 'preact';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import {Provider} from 'react-redux';
+
+import App from './components/App';
+import store, {history} from './store';
+
+import './style';
+
+export default class extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <BrowserRouter history={history}>
+          <Route path="/" component={App} />
+        </BrowserRouter>
+      </Provider>
+    );
+  }
+}
