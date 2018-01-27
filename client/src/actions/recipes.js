e25f66f5d76b0988df828bf06e27a2f2d0912da5
Larry Botha <larry@fixate.it>
2018-01-27 20:31:11 +0200

    create actions

---
+++ client/src/actions/recipes.js
@@ -0,0 +1,9 @@
+export const RECENT_RECIPES_REQUESTED = '@recipes/RECENT_RECIPES_REQUESTED';
+export const recentRecipesRequested = () => ({
+  type: RECENT_RECIPES_REQUESTED,
+});
+
+export const RECENT_RECIPES_SUCCEEDED = '@recipes/RECENT_RECIPES_SUCCEEDED';
+export const recentRecipesSucceeded = () => ({
+  type: RECENT_RECIPES_SUCCEEDED,
+});
