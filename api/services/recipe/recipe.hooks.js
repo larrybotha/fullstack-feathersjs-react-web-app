a968fe403c824d648c51c5f26ca44c026f34507a
Larry Botha <larry@fixate.it>
2018-01-29 09:07:02 +0200

    add annotations

--- api/services/recipe/recipe.hooks.js
+++ api/services/recipe/recipe.hooks.js
@@ -3,6 +3,10 @@ module.exports = {
     all: [],
     find: [],
     get: [],
+    // we add a create hook here we instead of relying on the client
+    // to send the createdAt date - we handle it on the server.
+    // Write the field straight to the data we have received from the
+    // client.
     create(context) {
       context.data.createdAt = new Date().getTime();
     },
