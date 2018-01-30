edb89e4c87b84f926b063885cc429be9e6ef084a
Larry Botha <larry@fixate.it>
2018-01-30 10:04:58 +0200

    add addRecipe actions and reducers

--- client/src/actions/recipes.js
+++ client/src/actions/recipes.js
@@ -4,9 +4,34 @@ export const requestRecentRecipes = () => ({
   type: RECENT_RECIPES_REQUEST,
 });
 
-export const RECENT_RECIPES_SUCCEEDED = '@recipes/RECENT_RECIPES_SUCCEEDED';
+export const RECENT_RECIPES_SUCCESS = '@recipes/RECENT_RECIPES_SUCCESS';
 // when we have a success response after requesting recipes.
 // This is fired inside the recipes saga on a successful response
 export const recentRecipesSucceeded = () => ({
-  type: RECENT_RECIPES_SUCCEEDED,
+  type: RECENT_RECIPES_SUCCESS,
 });
+
+// this is the action we dispatch from the view with the data we want to create
+// a new recipe with.
+// This data doesn't go through to the reducer - we don't want the reducer to
+// handle this yet, because then we'll have data in our store that doesn't come
+// from the database.
+// This is the action that our saga will subscribe to. Once our saga returns a
+// response, we should dispatch a new action from that saga from that saga indicating
+// success
+export const ADD_RECIPE = '@recipes/ADD';
+export const addRecipe = ({name, ingredients, description, imageUrl}) => ({
+  type: ADD_RECIPE,
+  name,
+  ingredients,
+  description,
+  imageUrl,
+});
+
+// this is the action we can dispatch. We don't need the function, because we're not
+// dispatching this from a view. We dispatch the action via our saga once our
+// recipe is successfully created.
+export const ADD_RECIPE_SUCCESS = '@recipes/ADD_SUCCESS';
+// export const addRecipeSucess = () => ({
+//   type: ADD_RECIPE_SUCCESS,
+// });
