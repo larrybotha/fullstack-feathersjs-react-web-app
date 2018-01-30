9ce716a4b7a74570de3b54658448f92118eb4230
Larry Botha <larry@fixate.it>
2018-01-30 10:04:21 +0200

    allow large files to be uploaded

--- api/app.js
+++ api/app.js
@@ -10,7 +10,6 @@ const configuration = require('@feathersjs/configuration');
 const express = require('@feathersjs/express');
 const socketio = require('@feathersjs/socketio');
 
-
 const middleware = require('./middleware');
 const services = require('./services');
 const appHooks = require('./app.hooks');
@@ -24,8 +23,13 @@ app.configure(configuration());
 app.use(cors());
 app.use(helmet());
 app.use(compress());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+
+// If we attempt to upload a file that is too big (what does too big mean), we
+// can specify a larger limit.
+// Why when uploading files and we have limit issues do we update the json property?
+app.use(express.json({limit: '10mb'}));
+app.use(express.urlencoded({extended: true}));
+
 app.use(favicon(path.join(app.get('public'), 'favicon.ico')));
 // Host the public folder
 app.use('/', express.static(app.get('public')));
@@ -43,7 +47,7 @@ app.configure(channels);
 
 // Configure a middleware for 404s and the error handler
 app.use(express.notFound());
-app.use(express.errorHandler({ logger }));
+app.use(express.errorHandler({logger}));
 
 app.hooks(appHooks);
 
