02e148ecaef1fbac4e84aa561cb9e2bf52221c1e
Larry Botha <larry@fixate.it>
2018-01-31 09:27:29 +0200

    add the single recipe route

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,16 +1,19 @@
 import {h, Component} from 'preact';
-import {Switch, Route} from 'react-router-dom';
+import {Switch, Redirect, Route} from 'react-router-dom';
 import {ConnectedRouter} from 'react-router-redux';
 
 import {history} from '../store';
 import Home from './routes/Home';
 import RecipesAdd from './routes/RecipesAdd';
+import SingleRecipe from './routes/SingleRecipe';
 
 const App = () => (
   <ConnectedRouter history={history}>
     <Switch>
       <Route exact path="/" component={Home} />
+      <Redirect exact from="/recipes" to="/" />
       <Route exact path="/recipes/add" component={RecipesAdd} />
+      <Route exact path="/recipes/:id" component={SingleRecipe} />
     </Switch>
   </ConnectedRouter>
 );
---
+++ client/src/components/routes/SingleRecipe.js
@@ -0,0 +1,73 @@
+import {h, Component} from 'preact';
+import {Header, Image, Segment, Container} from 'semantic-ui-react';
+import {connect} from 'react-redux';
+
+import Main from '../Main';
+
+import {fetchRecipe} from '../../actions/recipes';
+
+const uiStates = {
+  PENDING: 'pending',
+  SUCCESS: 'success',
+};
+
+class SingleRecipe extends Component {
+  state = {uiState: uiStates.PENDING};
+
+  componentWillMount() {
+    const {fetchRecipe, match} = this.props;
+
+    fetchRecipe(match.params.id);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (!this.props.recipe && nextProps.recipe) {
+      this.setState({uiState: uiStates.SUCCESS});
+    }
+  }
+
+  render() {
+    const {uiState} = this.state;
+    const {recipe} = this.props;
+
+    return (
+      <Main
+        render={() =>
+          uiState === uiStates.PENDING ? (
+            <div>loading...</div>
+          ) : (
+            <div>
+              <Header as="h1" textAlign="center">
+                {recipe.name}
+              </Header>
+
+              {recipe.imageUrl ? (
+                <Image size="medium" centered src={recipe.imageUrl} />
+              ) : null}
+
+              <Segment.Group>
+                {recipe.ingredients.map(i => <Segment key={i}>{i}</Segment>)}
+              </Segment.Group>
+
+              <Container>
+                {recipe.description
+                  .split('\n')
+                  .map((p, i) => <p key={i}>{p}</p>)}
+              </Container>
+            </div>
+          )
+        }
+      />
+    );
+  }
+}
+
+const mapStateToProps = ({recipes}) => ({
+  recipe: recipes.currentRecipe,
+});
+
+const mapDispatchToProps = {
+  fetchRecipe,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(SingleRecipe);
