c68d0ec048db2aa16fbc9a3922f6116fdac1d263
Larry Botha <larry@fixate.it>
2018-02-01 20:33:30 +0200

    neaten up index

--- api/app.js
+++ api/app.js
@@ -18,37 +18,39 @@ const channels = require('./channels');
 const app = express(feathers());
 
 // Load app configuration
-app.configure(configuration());
-// Enable CORS, security, compression, favicon and body parsing
-app.use(cors());
-app.use(helmet());
-app.use(compress());
+app
+  .configure(configuration())
 
-// If we attempt to upload a file that is too big (what does too big mean), we
-// can specify a larger limit.
-// Why when uploading files and we have limit issues do we update the json property?
-app.use(express.json({limit: '10mb'}));
-app.use(express.urlencoded({extended: true}));
+  // Enable CORS, security, compression, favicon and body parsing
+  .use(cors())
+  .use(helmet())
+  .use(compress())
 
-app.use(favicon(path.join(app.get('public'), 'favicon.ico')));
-// Host the public folder
-app.use('/', express.static(app.get('public')));
+  // If we attempt to upload a file that is too big (what does too big mean), we
+  // can specify a larger limit.
+  // Why when uploading files and we have limit issues do we update the json property?
+  .use(express.json({limit: '10mb'}))
+  .use(express.urlencoded({extended: true}))
 
-// Set up Plugins and providers
-app.configure(express.rest());
-app.configure(socketio());
+  .use(favicon(path.join(app.get('public'), 'favicon.ico')))
+  // Host the public folder
+  .use('/', express.static(app.get('public')))
 
-// Configure other middleware (see `middleware/index.js`)
-app.configure(middleware);
-// Set up our services (see `services/index.js`)
-app.configure(services);
-// Set up event channels (see channels.js)
-app.configure(channels);
+  // Set up Plugins and providers
+  .configure(express.rest())
+  .configure(socketio())
 
-// Configure a middleware for 404s and the error handler
-app.use(express.notFound());
-app.use(express.errorHandler({logger}));
+  // Configure other middleware (see `middleware/index.js`)
+  .configure(middleware)
+  // Set up our services (see `services/index.js`)
+  .configure(services)
+  // Set up event channels (see channels.js)
+  .configure(channels)
 
-app.hooks(appHooks);
+  // Configure a middleware for 404s and the error handler
+  .use(express.notFound())
+  .use(express.errorHandler({logger}))
+
+  .hooks(appHooks);
 
 module.exports = app;
