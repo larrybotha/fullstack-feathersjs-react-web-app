f83c75045775c2b7f23204854728e29645566921
Larry Botha <larry@fixate.it>
2018-02-01 09:21:05 +0200

    add user reducer to store

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,9 +2,11 @@ import {combineReducers} from 'redux';
 import {routerReducer} from 'react-router-redux';
 
 import recipesReducer from './recipes';
+import userReducer from './user';
 
 const reducer = combineReducers({
   recipes: recipesReducer,
+  user: userReducer,
   router: routerReducer,
 });
 
