20d000d9d4534d31639195699b8b4d2bdf496e35
Larry Botha <larry@fixate.it>
2018-02-01 21:20:34 +0200

    add annotations

--- client/src/services/user.js
+++ client/src/services/user.js
@@ -1,5 +1,15 @@
+// our createUser service which is called by our addUser saga handler, and which
+// passes through the username and password that the server will use to create a
+// user for us.
 export const createUser = (app, {username, password}) => {
+  // get the users service
   const users = app.service('users');
 
+  // send the request to create the user
+  // We return the data in the success response - this is what 'response' is
+  // assigned to in our addUser handler.
+  // What happens if the promise fails?
+  // Do we catch that in the saga?
+  // Surely we don't handle that here
   return users.create({email: username, password}).then((data, err) => data);
 };
