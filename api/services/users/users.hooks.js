228a21b58c9a3120a75641d579da06c7ebc28c9c
Larry Botha <larry@fixate.it>
2018-02-03 17:42:04 +0200

    add log in service

--- api/services/users/users.hooks.js
+++ api/services/users/users.hooks.js
@@ -1,24 +1,30 @@
-
+const authentication = require('@feathersjs/authentication');
+const local = require('@feathersjs/authentication-local');
 
 module.exports = {
   before: {
     all: [],
-    find: [],
+    // this will use jwt to authenticate requests, or something. Don't really know
+    // what this is.
+    find: [authentication.hooks.authenticate('jwt')],
     get: [],
-    create: [],
+    // this will hash the password on creation of a user
+    create: [local.hooks.hashPassword('jwt')],
     update: [],
     patch: [],
-    remove: []
+    remove: [],
   },
 
   after: {
-    all: [],
+    // prevent password being sent back in responses, apparently. Didn't get this
+    // to work.
+    all: [local.hooks.protect('password')],
     find: [],
     get: [],
     create: [],
     update: [],
     patch: [],
-    remove: []
+    remove: [],
   },
 
   error: {
@@ -28,6 +34,6 @@ module.exports = {
     create: [],
     update: [],
     patch: [],
-    remove: []
-  }
+    remove: [],
+  },
 };
