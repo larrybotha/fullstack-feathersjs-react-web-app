581ec116f1ea3048831258d194dfd09b83c7453f
Larry Botha <larry@fixate.it>
2018-02-04 17:47:36 +0200

    add logout saga

--- client/src/sagas/user.js
+++ client/src/sagas/user.js
@@ -3,8 +3,10 @@ import {call, put} from 'redux-saga/effects';
 
 import {history} from '../store';
 
-import * as actions from '../actions/user';
-import {createUser, login} from '../services/user';
+import * as actions from '../actions/currentUser';
+import * as userService from '../services/user';
+
+import routes from '../routes';
 
 // the generator we call that makes fires the function that makes the request
 // to our users service
@@ -14,7 +16,10 @@ import {createUser, login} from '../services/user';
 const addUser = function* addUser(feathersApp, {email, password}) {
   // assign the response we get from the server after making a request to create
   // a user
-  const response = yield call(createUser, feathersApp, {email, password});
+  const response = yield call(userService.createUser, feathersApp, {
+    email,
+    password,
+  });
 
   // once we have that response, we can call our action creator with the details
   // that we want stored in state
@@ -24,7 +29,7 @@ const addUser = function* addUser(feathersApp, {email, password}) {
   // result), we send the user to the login page. This is yielded... doesn't need
   // to, because this the history API will move us anyways, but probably good
   // practice to keep our generators consistent
-  yield history.push('/login');
+  yield history.push(routes.login);
 };
 
 // create a saga for adding users
@@ -42,15 +47,18 @@ export const addUserSaga = function* addUserSaga(feathersApp) {
 const tryLogin = function* tryLogin(feathersApp, {email, password}) {
   // we store the user we get in the response from making a request against
   // our API
-  const {user} = yield call(login, feathersApp, {email, password});
+  const response = yield call(userService.login, feathersApp, {
+    email,
+    password,
+  });
 
   // if we have a use
-  if (user) {
+  if (response.accessToken) {
     // dispatch the loginSuccess action, sending through the user data returned
-    yield put(actions.loginSuccess({currentUser: user}));
+    yield put(actions.loginSuccess({currentUser: response}));
 
     // and then redirect the user to the home
-    yield history.push('/');
+    yield history.push(routes.home);
   } else {
     // otherwise dispatch loginFailure
     yield put(actions.loginFailure());
@@ -63,4 +71,23 @@ export const loginUserSaga = function* loginUserSaga(feathersApp) {
   // The tryLogin handler is fired after this, which receives the data from the
   // originally dispatched action
   yield* takeEvery(actions.USER_LOGIN_REQUEST, tryLogin, feathersApp);
+};
+
+// create handler for our logoutSaga
+const requestLogout = function* requestLogout(feathersApp) {
+  // we call logout from the user service
+  yield call(userService.logout, feathersApp);
+
+  // when it returns we dispatch logout success
+  // Do we need to handle a failure? What if the server is down?
+  yield put(actions.logoutSuccess());
+  // we redirect user to log in form on log out
+  yield history.push(routes.login);
+};
+
+// this saga subscribes to hte USER_LOGOUT_REQUEST action
+export const logoutUserSaga = function* logoutUserSaga(feathersApp) {
+  // and it fires its requestLogout handler, passing in our app, and whatever the
+  // action was dispatched with, if anything
+  yield* takeEvery(actions.USER_LOGOUT_REQUEST, requestLogout, feathersApp);
 };
