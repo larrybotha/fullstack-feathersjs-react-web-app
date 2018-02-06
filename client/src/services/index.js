23283e6bf57dccb00a945beea2eef1af8eef9fa8
Larry Botha <larry@fixate.it>
2018-02-06 09:16:14 +0200

    fix typo - store should be storage in authentication config

--- client/src/services/index.js
+++ client/src/services/index.js
@@ -6,14 +6,15 @@ import authentication from '@feathersjs/authentication-client';
 const host = 'http://localhost:3030';
 
 // we need to create our client-side feathers app
-const createApp = () => feathers()
-  // we configure it specifying the type of application it is - a RESTful app,
-  // and how we want to make requests. We want to use the fetch API, and I'm assuming
-  // that the fetch polyfill we pass in is purely there for browsers which don't yet
-  // support fetch.
-  // In fact, no - if I don't provide fetch as a param the UI errors out.
-  .configure(rest(host).fetch(fetch))
-  // .configure(feathers.hooks())
-  .configure(authentication({store: window.localStorage}));
+const createApp = () =>
+  feathers()
+    // we configure it specifying the type of application it is - a RESTful app,
+    // and how we want to make requests. We want to use the fetch API, and I'm assuming
+    // that the fetch polyfill we pass in is purely there for browsers which don't yet
+    // support fetch.
+    // In fact, no - if I don't provide fetch as a param the UI errors out.
+    .configure(rest(host).fetch(fetch))
+    // .configure(feathers.hooks())
+    .configure(authentication({storage: window.localStorage}));
 
 export default createApp;
