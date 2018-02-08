6a9161df3bdf628fdfb9dd29c28c90da9ee775d4
Larry Botha <larry@fixate.it>
2018-02-08 09:41:59 +0200

    add an auth component and wrap recipes add in it

--- client/src/components/routes/RecipesAdd.js
+++ client/src/components/routes/RecipesAdd.js
@@ -1,8 +1,16 @@
-import {h, Component} from 'preact';
+import {h} from 'preact';
 
 import Main from '../Main';
 import AddRecipe from '../forms/AddRecipe';
+import Auth from '../Auth';
 
-const RecipesAdd = () => <Main render={() => <AddRecipe />} />;
+const RecipesAdd = props => (
+  <Auth
+    {...props}
+    render={renderProps => (
+      <Main render={() => <AddRecipe {...renderProps} />} />
+    )}
+  />
+);
 
 export default RecipesAdd;
