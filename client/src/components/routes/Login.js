73d2b10c9748916fff560b909590b9c817536d15
Larry Botha <larry@fixate.it>
2018-02-08 09:43:15 +0200

    send location to form

--- client/src/components/routes/Login.js
+++ client/src/components/routes/Login.js
@@ -3,15 +3,17 @@ import {h} from 'preact';
 import Main from '../Main';
 import Login from '../forms/Login';
 
-const LoginRoute = () => (
-  <Main
-    render={() => (
-      <div>
-        <h1>Log in</h1>
-        <Login />
-      </div>
-    )}
-  />
-);
-
+const LoginRoute = props => {
+  const {location} = props;
+  return (
+    <Main
+      render={() => (
+        <div>
+          <h1>Log in</h1>
+          <Login location={location} />
+        </div>
+      )}
+    />
+  );
+};
 export default LoginRoute;
