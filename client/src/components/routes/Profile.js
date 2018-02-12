39a63f3c70b0a1d858fdb2504edc1c83fd53e1a7
Larry Botha <larry@fixate.it>
2018-02-12 09:01:00 +0200

    begin profile page

---
+++ client/src/components/routes/Profile.js
@@ -0,0 +1,17 @@
+import {h} from 'preact';
+
+import Main from '../Main';
+import RecipeList from '../RecipeList';
+
+const Home = () => (
+  <Main
+    render={({requestRecentRecipes, recipes}) => (
+      <RecipeList
+        requestRecentRecipes={requestRecentRecipes}
+        recipes={recipes}
+      />
+    )}
+  />
+);
+
+export default Home;
